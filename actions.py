8cfe62807aa96bf7b0bfb1b0418b2025b365f4ce
Sarthak Sharma <ss22072000@gmail.com>
2020-06-07 01:47:34 -0400

    Add files via upload

--- actions.py
+++ actions.py
@@ -24,23 +24,31 @@ class ActionQueryCoronaLocation(Action):
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
              cursor = mydb.cursor()
-             entities = tracker.latest_message['entities']
-             for entity in entities:
-                 if(entity['entity'] == 'location'):
-                     location = entity['value']
+             try:
+                 entities = tracker.latest_message['entities']
+                 for entity in entities:
+                     if(entity['entity'] == 'location'):
+                         location = entity['value']
+            except:
+                dispatcher.utter_message('I am not able to understand what you are trying to say')
 
              cursor.execute("USE CoronaVirus")
              cursor.execute("SELECT * from state_stats WHERE state=%s",(location,))
              result = cursor.fetchall()
              if(len(result) == 0):
-                 print("I got executed")
                  cursor.execute("SELECT * from district_stats WHERE district=%s",(location,))
                  result = cursor.fetchall()
                  if(len(result) != 0):
                      for row in result:
                          dispatcher.utter_message("-----------------\n[District] : "+location+"\n[State] : "+row[1]+"\n[Active Cases] :"+str(row[2])+"\n[Confirmed Cases] :"+str(row[3])+"\n[TotalRecovered] :"+str(row[4])+"\n[Total Deaths] :"+str(row[5]))
                  else:
-                    dispatcher.utter_message("I was not able to find any information about the provided location.")
+                     cursor.execute("SELECT * from country_stats WHERE country=%s",(location,))
+                     result = cursor.fetchall()
+                     if(len(result) != 0):
+                         for row in result:
+                             dispatcher.utter_message("------------------\n[Country] : "+location+"\n[New Confirmed Cases] :"+str(row[1])+"\n[Total Confirmed Cases] : "+str(row[2])+"\n[New Deaths] :"+str(row[3])+"\n[Total Deaths] :"+str(row[4])+"\n[New Recovered] :"+str(row[5])+"\n[Total Recovered] :"+str(row[6])+"\n[Source] :"+row[7])
+                     else:
+                         dispatcher.utter_message("I was not able to find any information about the provided location.")
              else:
                  for row in result:
                      dispatcher.utter_message("-----------------\n[State] :"+row[0]+"\n[Active Cases] :"+str(row[1])+"\n[Confirmed Cases] :"+str(row[2])+"\n[Total Recovered] :"+str(row[3])+"\n[Total Deaths] :"+str(row[4])+"\n[Source] :"+row[5])
